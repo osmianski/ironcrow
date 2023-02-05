05988e17811d50adf68c02c5f02865bb6f2ce2a2
Vlad Osmianski <vlaosm@gmail.com>
2023-02-05 14:07:44 +0200

    Guest home page with placeholder content

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,12 +1,24 @@
 <script setup>
 import { Head } from '@inertiajs/inertia-vue3';
+import GuestLayout from "@/Components/GuestLayout.vue";
 </script>
 
 <template>
-    <Head>
-        <title>Hello, world!</title>
-    </Head>
-    <main class="fixed inset-0 grid place-items-center">
-        <h1 class="text-2xl">Hello, world!</h1>
-    </main>
+    <GuestLayout>
+        <Head title="Home page will be here" />
+        <main class="mx-auto max-w-7xl p-6">
+            <h1 class="text-2xl text-center">
+                An awesome home page will be here!
+            </h1>
+            <p class="text-center">
+                However, it's not there yet. I'm just getting started!
+            </p>
+            <p class="text-center">
+                <img class="inline w-1/2" src="/images/start-business.svg">
+            </p>
+            <p class="text-center">
+                <a href="https://iconscout.com/illustrations/start-business" target="_blank">Start business Illustration</a> by <a href="https://iconscout.com/contributors/pixel-true-designs">Pixel True</a> on <a href="https://iconscout.com">IconScout</a>
+            </p>
+        </main>
+    </GuestLayout>
 </template>
