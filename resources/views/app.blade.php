826b19721a2d217b4d18dd7e3711c672aaee4a56
Vlad Osmianski <vlaosm@gmail.com>
2023-02-05 13:44:41 +0200

    Tailwind UI prerequisites

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport"
           content="width=device-width, initial-scale=1">
 
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
     @vite('resources/js/app.js')
     @inertiaHead
 </head>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
     content: [
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.vue",
     ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
 }
